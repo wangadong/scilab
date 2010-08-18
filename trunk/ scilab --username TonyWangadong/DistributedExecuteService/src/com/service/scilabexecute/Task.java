2b7426d09de7b3c134b0ceccdc37a7276d623070
TonyWangadong <TonyWangadong@eaff462e-2b3b-8e23-d5b4-97c0f2870c3e>
2010-08-18 08:04:06 +0000

    添加注释

--- trunk/ scilab --username TonyWangadong/DistributedExecuteService/src/com/service/scilabexecute/Task.java
+++ trunk/ scilab --username TonyWangadong/DistributedExecuteService/src/com/service/scilabexecute/Task.java
@@ -1,24 +1,45 @@
 package com.service.scilabexecute;
 
-import java.io.File;
-
+/**
+ * 可执行任务实体，调用命令行管道命令保存任务结果到文本文件
+ * 
+ * @author wangadong
+ * @version 1.2
+ * @see ScilabService
+ */
 public class Task implements Runnable {
-	private String codes;
-	private String savePath;
-	private String hostIP;
+	private String codes;// scilab任务代码
+	private String savePath;// 结果保存路径
+	private String hostIP;// 服务器主机IP地址
+
+	/**
+	 * 任务构造函数
+	 * 
+	 * @param codes
+	 *            scilab任务代码
+	 * @param savePath
+	 *            结果保存路径
+	 * @param hostIP
+	 *            服务器主机IP地址
+	 */
 	public Task(String codes, String savePath, String hostIP) {
 		this.codes = codes;
 		this.savePath = savePath;
-		this.hostIP=hostIP;
+		this.hostIP = hostIP;
 	}
 
+	/**
+	 * 调用windows命令行的管道命令保存scilab执行结果<br>
+	 * 运行execute.jar计算包，并传入必要参数
+	 */
 	public void run() {
 		try {
-			Process process = Runtime.getRuntime().exec("cmd /c java -jar C:/ScilabDistribution/execute.jar"+ " \"" + codes+ "\" "+"\"" + savePath + "\" " + "\""
-					+ hostIP + "\">\"" +savePath+"\"" );
-
+			@SuppressWarnings("unused")
+			Process process = Runtime.getRuntime().exec(
+					"cmd /c java -jar C:/ScilabDistribution/execute.jar"
+							+ " \"" + codes + "\" " + "\"" + savePath + "\" "
+							+ "\"" + hostIP + "\">\"" + savePath + "\"");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
