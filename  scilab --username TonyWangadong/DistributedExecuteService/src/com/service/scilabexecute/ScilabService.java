dd2b1bacff553f8049a8f29fdc8a8c886a39a61c
TonyWangadong <TonyWangadong@eaff462e-2b3b-8e23-d5b4-97c0f2870c3e>
2010-08-18 08:04:06 +0000

    添加注释

---  scilab --username TonyWangadong/DistributedExecuteService/src/com/service/scilabexecute/ScilabService.java
+++  scilab --username TonyWangadong/DistributedExecuteService/src/com/service/scilabexecute/ScilabService.java
@@ -6,22 +6,39 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
+/**
+ * Scilab分页式计算服务主体
+ * 
+ * @author wangadong
+ * @version 1.0
+ * @see Task
+ * @see DistributedService
+ */
 public class ScilabService implements Executor {
-	private static final ScilabService _INSTANCE = new ScilabService();
-	final Queue<Runnable> tasks;
-	final Executor executor;
+	private static final ScilabService _INSTANCE = new ScilabService();// singleton模式
+	final Queue<Runnable> tasks;// 分布式节点任务队列
+	final Executor executor;// 任务执行器
 	Runnable active;
 
+	/**
+	 * 构造函数
+	 */
 	public ScilabService() {
-		tasks = new LinkedBlockingQueue<Runnable>();
-		int cpuCoreNumber = Runtime.getRuntime().availableProcessors();   
-        executor = Executors.newFixedThreadPool(cpuCoreNumber);		
+		tasks = new LinkedBlockingQueue<Runnable>();// 初始化任务队列
+		int cpuCoreNumber = Runtime.getRuntime().availableProcessors();// 获取本机CPU核心数
+		executor = Executors.newFixedThreadPool(cpuCoreNumber);// 创建以CPU核心数为准的任务执行线程池
 	}
 
+	/**
+	 * 获取singleton实例
+	 */
 	public synchronized static ScilabService getInstance() {
 		return _INSTANCE;
 	}
 
+	/**
+	 * 执行队列中的任务
+	 */
 	public synchronized void execute(final Runnable r) {
 		tasks.offer(new Runnable() {
 			public void run() {
@@ -37,14 +54,28 @@ public class ScilabService implements Executor {
 		}
 	}
 
+	/**
+	 * 判断当前任务是否完成，若完成则执行下一个任务
+	 */
 	protected synchronized void scheduleNext() {
 		if ((active = tasks.poll()) != null) {
 			executor.execute(active);
 		}
 	}
 
+	/**
+	 * 提交任务到任务队列
+	 * 
+	 * @param codes
+	 *            任务代码
+	 * @param savePath
+	 *            结果保存路径
+	 * @param hostIP
+	 *            服务器主机IP地址
+	 */
 	public void submitTask(String codes, String savePath, String hostIP) {
-		Task currentTask = new Task(codes, savePath, hostIP);
+		Task currentTask = new Task(codes, savePath, hostIP);// 创建新任务
+		// 创建保存路径目录
 		File myFilePath = new File(savePath).getParentFile();
 		if (!myFilePath.exists()) {
 			if (myFilePath.mkdirs())
@@ -52,18 +83,22 @@ public class ScilabService implements Executor {
 		} else {
 			if (deleteFile(myFilePath))
 				System.out.println("删除目录");
-			;
 			if (!myFilePath.exists())
 				System.out.println("更新成功");
 			myFilePath.mkdirs();
 		}
-		execute(currentTask);
+		execute(currentTask);// 执行当前任务
 	}
 
+	/**
+	 * 获取当前分布式节点的任务数
+	 * 
+	 */
 	public int getTaskNum() {
 		return this.tasks.size();
 	}
 
+	// 删除文件及文件夹
 	public boolean deleteFile(File f) {
 		if (f.exists()) {
 			if (f.isFile())
